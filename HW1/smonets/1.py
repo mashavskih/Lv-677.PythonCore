c5e36447e01f13116105f961f62fb1add6432c19
smonets <s.monets@gmail.com>
2022-03-18 12:22:50 +0200

    added HW1 1

---
+++ HW1/smonets/1.py
@@ -0,0 +1,7 @@
+answer_name = input("What is your name? ")
+answer_age = input("How old are you? ")
+answer_city = input("Where do you live? ")
+
+print(f"""Hello, {answer_name},
+Your age is {answer_age},
+You live in {answer_city}.""")
