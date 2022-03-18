411720b92d52f4d07c07d33414a962214250f305
smonets <s.monets@gmail.com>
2022-03-18 12:29:10 +0200

    added HW1_2

---
+++ HW1/smonets/2.py
@@ -0,0 +1,8 @@
+a = int(input("Введіть змінну a: "))
+b = int(input("Введіть змінну b: "))
+
+print("a + b =",a + b)
+print("a - b =",a - b)
+print("a * b =",a * b)
+print("a / b =",a / b)
+print("a ** b =",a ** b)
