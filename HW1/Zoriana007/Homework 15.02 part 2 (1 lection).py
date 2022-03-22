133fb031146d8526b89d50a2026dc7b54cc93934
Zoriana Matsko <zoriana.matsko@gmail.com>
2022-03-23 00:49:09 +0100

    Added folder and task for HW

---
+++ HW1/Zoriana007/Homework 15.02 part 2 (1 lection).py
@@ -0,0 +1,25 @@
+#version with stable data
+
+print("Hi! Let's have stable variables a and b.")
+user = input("Enter your name ")
+a = 4
+b = 67
+print("a+b = ", a+b)
+print("a-b = ", a-b)
+print("a*b = ", a*b)
+print("a/b = ", a/b)
+print("a**b = ", a**b)
+print("Nice")
+print("Thank you for your help, ", user)
+
+# version with data input, not a stable data 
+
+a = int(input("Please provide number for a "))
+print("a = ", a)
+b = int(input("Please provide number for b "))
+print("b = ", b)
+print("a+b = ", a - b)
+print("a-b = ", a-b)
+print("a*b = ", a*b)
+print("a/b = ", a/b)
+print("a**b = ", a**b)
