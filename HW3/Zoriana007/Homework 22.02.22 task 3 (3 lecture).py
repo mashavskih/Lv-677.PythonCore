19852681214807d87f17cd279d83da9a66a5eb07
Zoriana Matsko <zoriana.matsko@gmail.com>
2022-03-24 00:54:03 +0100

    Adding HW to folder

---
+++ HW3/Zoriana007/Homework 22.02.22 task 3 (3 lecture).py
@@ -0,0 +1,6 @@
+first_var = 45
+second_var = 89
+print("first_var = ", first_var, "second_var = ", second_var)
+print("Let's switch value of first_var and second_var")
+first_var, second_var = second_var, first_var
+print("first_var = ", first_var, "second_var = ", second_var)
