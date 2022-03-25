d104a77f19051fac6105b0e96a06a88029aed2f0
Alexis Taran <alexistaran@gmail.com>
2022-03-25 06:16:10 +0200

    added new file:   Task1.1.py, Task1.2.py, Zen.txt

---
+++ HW1/AlexisTaran/Task1.1.py
@@ -0,0 +1,7 @@
+name = input('What is your name?')
+age = input ('How old are you?')
+city = input('Where do you live?')
+print('Hello,', str.title(name))
+print('Your age is', (age))
+print('You live in', str.title(city))
+print(name, age, city)
---
+++ HW1/AlexisTaran/Task1.2.py
@@ -0,0 +1,3 @@
+a = int(input("input you number:"))
+b = int(input("input you number:"))
+print("Product:",a+b, "Remainder:",a-b, "Multiplication:",a*b, "Division:",a/b, "Power:",a**b, sep='\n')
