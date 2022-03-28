724142c442ab8c7ce5c65ae7f2bf77cbc4852030
Makarowkaa <krats1904@gmail.com>
2022-03-28 20:56:32 +0300

    reset

--- HW_5/HW_5_2.py
+++
@@ -1,9 +0,0 @@
-username = input("Enter your username: ")
-while username:
-    if username != "First":
-        print("Wrong username\n")
-        username = input("Enter your username again: ")
-    else:
-        print(f"Hello, {username}")
-        break
-input()
