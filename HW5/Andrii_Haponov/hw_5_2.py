34e228d1f8af98b7576c0928c0563698bfb3a4d3
haponovaw@gmail.com <gaponovaw@gmail.com>
2022-03-28 13:30:18 +0300

    push HW 5, Task 2 finish

--- HW5/Andrii_Haponov/hw_5_2.py
+++ HW5/Andrii_Haponov/hw_5_2.py
@@ -5,19 +5,6 @@
 # Задача2. Напишите скрипт, проверяющий логин, который вводит пользователь.
 # Если логин «Первый», то приветствуйте пользователей. Если логин другой, отправьте сообщение об ошибке.
 # (необходимо использовать цикл while)
-
-# checks_the_login = []
-# item = input("Enter login : ")
-# while item != "finish":
-#     checks_the_login.append(item)
-#     item = input("Enter login : ")
-# print(checks_the_login)
-
-# login = input("Enter login : ")
-# if login in checks_the_login:  
-#     print(f"Hello {login}!")
-# else:    
-#     print("Error: This login was not found.")
        
 
 login = input("Enter login : ")  
