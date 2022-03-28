a7157c470235014b3e809aab7c51cbc9e40a4943
haponovaw@gmail.com <gaponovaw@gmail.com>
2022-03-28 13:27:09 +0300

    push HW 5, Task 2 new while

--- HW5/Andrii_Haponov/hw_5_2.py
+++ HW5/Andrii_Haponov/hw_5_2.py
@@ -6,20 +6,25 @@
 # Если логин «Первый», то приветствуйте пользователей. Если логин другой, отправьте сообщение об ошибке.
 # (необходимо использовать цикл while)
 
-checks_the_login = []
-item = input("Enter login : ")
-while item != "finish":
-    checks_the_login.append(item)
-    item = input("Enter login : ")
-print(checks_the_login)
+# checks_the_login = []
+# item = input("Enter login : ")
+# while item != "finish":
+#     checks_the_login.append(item)
+#     item = input("Enter login : ")
+# print(checks_the_login)
 
-login = input("Enter login : ")
-if login in checks_the_login:  
-    print(f"Hello {login}!")
-else:    
-    print("Error: This login was not found.")
+# login = input("Enter login : ")
+# if login in checks_the_login:  
+#     print(f"Hello {login}!")
+# else:    
+#     print("Error: This login was not found.")
        
 
-
+login = input("Enter login : ")  
+while login != "First":
+    print("Error")
+    login = input("Enter login : ")
+else:
+    print("Hello First!")
 
 
