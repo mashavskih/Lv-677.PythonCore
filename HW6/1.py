2ce94c448af92475701a6a773ddab0c8d3f07654
smonets <s.monets@gmail.com>
2022-03-30 11:38:30 +0300

    finished HW6_1

---
+++ HW6/1.py
@@ -0,0 +1,9 @@
+def largest_num(num1, num2):
+    """ returns largers number of two numbers """
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+    
+
+print(largest_num(10, 999))
