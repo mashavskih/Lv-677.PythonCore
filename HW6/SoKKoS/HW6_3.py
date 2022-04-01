64513fb16ae4fbc1c762dff9eb9cc4d18f5a4666
7SoKKoS7 <97355065+7SoKKoS7@users.noreply.github.com>
2022-04-02 01:49:36 +0300

    add HW6_* check

--- HW6/SoKKoS/HW6_3.py
+++ HW6/SoKKoS/HW6_3.py
@@ -1,12 +1,14 @@
+def func_dict(input_txt):
+    output_list = list(input_txt)
+    output_dict = {}
+    for i in output_list:
+        output_dict[i] = input_txt.count(i)
+    return output_dict
+
+
 input_txt = input("Enter string - ")
-output_list = list(input_txt)
-output_dict = {}
 
-for i in output_list:
-    output_dict[i] = input_txt.count(i)
-
-
-print(f'Output {output_dict}')
+print(f'Output {func_dict(input_txt)}')
 
 
 
