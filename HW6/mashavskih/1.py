b4bf1f8cbca6e685e2741ff5622865c62a210aa4
Baliun Mariya <mashavscix967@gmail.com>
2022-04-03 11:56:36 +0300

    added HW6-1

---
+++ HW6/mashavskih/1.py
@@ -0,0 +1,18 @@
+first_number = int(input('Enter first number: '))
+second_number = int(input('Enter second number: ')) 
+
+def largest_number():
+    """The function that returns 
+    the largest number of two numbers """
+
+    if first_number > second_number:
+        print(f'{first_number}  largest than {second_number}')
+    elif first_number < second_number:
+        print(f'{second_number} largest than {first_number}')
+    else:
+        print(f'{first_number} equal {second_number}')
+largest_number()
+
+
+
+
