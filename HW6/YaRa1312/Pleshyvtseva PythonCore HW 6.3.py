9b58419bbee618075cb9ea7d494a6640afce7918
YaRa1312 <96065857+YaRa1312@users.noreply.github.com>
2022-04-03 12:57:52 +0200

    Update Pleshyvtseva PythonCore HW 6.3.py
    I forgot to write documentation and added it here

--- HW6/YaRa1312/Pleshyvtseva PythonCore HW 6.3.py
+++ HW6/YaRa1312/Pleshyvtseva PythonCore HW 6.3.py
@@ -1,6 +1,11 @@
 # 6.3
 
 def characters_number_calculation(user_string):
+    '''
+    The function calculates characters in the string
+    entered by a user and give the quality of usage of each
+    character in a dict where {character: quality of usage}
+    '''
     result = {}
     for i in user_string:
         result[i] = user_string.count(i)
