74d13920642dde8857135afe633cc49c3f0c5b9b
tymoschuk.mail@gmail.com <macbookpro@yt.local>
2022-04-04 16:55:46 +0300

    Added Katas

--- HW6/ytmsck/HW6_3.py
+++ HW6/ytmsck/HW6_3.py
@@ -1,17 +1,6 @@
-mystring = "Hello"
-
 def number_of_charachters(your_string):
+    """
+    Function creates dictionary where key is an element of a string
+    and value counts how many times does an element included to a string 
+    """
     return {x: your_string.count(x) for x in your_string}
-
-print(number_of_charachters('gfvgcvxdx kjhjv 1n43!'))
-
-# def calculate_characters(some_string: str) -> dict:
-#     """ 
-#     Function calculates the number of characters included in a given string 
-#     and returns dict where keys are unique characters of given string
-#     and values are number of relevant character of given string
-#     """
-#     return {char: some_string.count(char) for char in some_string}
-
-
-# print(calculate_characters("gfvgcvxdx kjhjv 1n43!"))
