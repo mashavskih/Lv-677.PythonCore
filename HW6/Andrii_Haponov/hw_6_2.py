0f06b68f4da3b5fc3e5b0309668df9237a3a468a
haponovaw@gmail.com <gaponovaw@gmail.com>
2022-04-04 19:18:02 +0300

    push HW6_2

--- HW6/Andrii_Haponov/hw_6_2.py
+++ HW6/Andrii_Haponov/hw_6_2.py
@@ -5,3 +5,32 @@
 # Задача2. Написать программу, вычисляющую площадь прямоугольника, треугольника и круга
 # (напишите три функции для вычисления квадрата,
 # и вызывать их в основной программе в зависимости от выбора пользователя).
+
+import math
+
+def erea_rectangle(a, b):
+    p_rectangle = a*b
+    return p_rectangle
+
+def erea_triangle(c, h):
+    p_triangle = c * h * 0.5
+    return p_triangle
+
+def erea_circle(r):
+    p_circle = math.pi * r**2
+    return p_circle
+
+rectangle = int(input(
+                "Enter 1 if calculates  the area of a rectangle,\
+                2 if calculates the area of a triangle and \
+                3 if calculates the area of a circle : "
+                ))
+
+if rectangle == 1:
+    print(erea_rectangle(int(input("Enter the side 1: ")), int(input("Enter the side 1: "))))
+elif rectangle == 2 :
+    print(erea_triangle(int(input("Enter the triangle height: ")), int(input("Enter the base of a triangle: "))))
+elif rectangle == 3:
+    print(erea_circle(int(input("Enter the circle radius: "))))
+else:
+    print("Error you dont etner True numder !")
